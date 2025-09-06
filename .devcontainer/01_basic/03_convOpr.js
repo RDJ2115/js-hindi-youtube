7639e7a254a9e716c7fb13a9c410b991e88248dc
RDJ2115 <143861792+RDJ2115@users.noreply.github.com>
2025-09-06 19:31:38 +0000

    Conversion of Operations

--- .devcontainer/01_basic/03_convOpr.js
+++ .devcontainer/01_basic/03_convOpr.js
@@ -5,7 +5,19 @@
 
 // let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//***Operations */
+let value = 3
+let negValue = -value
+//console.log(negValue);
+//"33abc" =>NaN
+//true=> 1 ; false=> 0
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+ //1:30:00
