c1a6709601e948e267bc83d930a0f0eabd4df6fa
RDJ2115 <143861792+RDJ2115@users.noreply.github.com>
2025-09-07 05:36:20 +0000

    datatypes

---
+++ .devcontainer/01_basic/dttypes-sum.js
@@ -0,0 +1,27 @@
+// Primitive
+
+// 7 types : String, Number , boolean, null, undefined,bigint
+
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+// const id = Symbol('123')
+// const id = Symbol('123')
+//Reference (non primitive)
+
+//arrays , objects, functions
+
+const heroes = ["shaktiman", "nagraj", "krish"]
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+console.log(typeof heroes);
+
+
+ 
